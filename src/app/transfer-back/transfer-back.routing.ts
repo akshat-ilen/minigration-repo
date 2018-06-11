99b6be117cd24e06ce2494cce5a4c6fe21c8c43b
Akshat Jain <akshatjain1995.aj@gmail.com>
2018-06-11 10:14:08 +0530

    Merge changes

---
+++ src/app/transfer-back/transfer-back.routing.ts
@@ -0,0 +1,16 @@
+import { TransferBackComponent } from './transfer-back.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '', component: TransferBackComponent,
+    data: {
+      meta: {
+        title: 'back-http.title',
+        description: 'back-http.text'
+      }
+    },
+  },
+];
+
+export const TransferBackRoutes = RouterModule.forChild(routes);
